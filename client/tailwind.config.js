0f5f361155b2ddc9f0a2ddf6d00e5581064ce469
Femzy1900 <adedokunfemi14@gmail.com>
2025-05-07 04:05:40 +0100

    Update package.json and package-lock.json to include ethers and react-router-dom dependencies; modify tailwind.config.js to specify content paths.

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
