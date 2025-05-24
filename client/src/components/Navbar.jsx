2724ffef9cccec85691d92f295a14803aa3620e7
Femzy1900 <adedokunfemi14@gmail.com>
2025-05-24 02:36:41 +0100

    feat: update Navbar component to link to profile and enhance drawer functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -37,11 +37,11 @@ const Navbar = () => {
             else "connect()";
           }}
         />
-        <Link to="/" className="flex gap-2 items-center">
+        <Link to="/profile" className="flex gap-2 items-center">
           <div className="h-[52px] w-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
             <img
               src={thirdweb}
-              alt="logo"
+              alt="user"
               className="w-[60%] h-[60%] object-contain"
             />
           </div>
@@ -52,7 +52,7 @@ const Navbar = () => {
       <div className="sm:hidden flex justify-between items-center relative">
         <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
           <img
-            src={thirdweb}
+            src={logo}
             alt="user"
             className="w-[60%] h-[60%] object-contain"
           />
@@ -60,13 +60,57 @@ const Navbar = () => {
             src={menu}
             alt="menu"
             className="w-[34px] h-[34px] object-contain cursor-pointer"
-            onClick={() => setToggleDrawer(!toggleDrawer)}
+            onClick={() => setToggleDrawer((prev) => !prev)}
           />
 
           <div
-            className={`absolute top-[60px] right-0 bg-[#1c1c24] shadow-secondary ${toggleDrawer} ? "-translate-y-[100px]" : "translate-y-0" 
+            className={`absolute top-[60px] right-0 bg-[#1c1c24] z-10 shadow-secondary ${toggleDrawer ? "-translate-y-[100px]" : "translate-y-0"}
             transition-all duration-700 py-4`}
-          ></div>
+          >
+            <ul className="mb-4">
+              {navlinks.map((link) => (
+                <li
+                  key={link.name}
+                  className={`flex p-4 ${
+                    isActive === link.name && "bg-[#3a3a43]"
+                  } cursor-pointer`}
+                  onClick={() => {
+                    setIsActive(link.name);
+                    setToggleDrawer(false);
+                    navigate(link.link);
+                  }}
+                >
+                  <img
+                    src={link.imgUrl}
+                    alt={link.name}
+                    className={`w-[24px] h-[24px] object-contain ${
+                      isActive === link.name ? "grayscale" : "grayscale-0"
+                    }`}
+                  />
+                  <p
+                    className={`font-epilogue font-semibold text-[14px] leading-[26px]   ml-[20px] ${
+                      isActive === link.name
+                        ? "text-[#1dc071]"
+                        : "text-[#808191]"
+                    }`}
+                  >
+                    {link.name}
+                  </p>
+                </li>
+              ))}
+            </ul>
+            <div className="flex mx-4">
+              <CustomButton
+                btnType="button"
+                title={address ? "Create a Campaign" : "Connect"}
+                styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+                handleClick={() => {
+                  if (address) navigate("/create-campaign");
+                  else "connect()";
+                }}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
