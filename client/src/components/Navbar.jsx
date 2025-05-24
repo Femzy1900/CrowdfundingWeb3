9171f625aec9ee758d9d070d977b21ca4af1e041
Femzy1900 <adedokunfemi14@gmail.com>
2025-05-24 02:20:23 +0100

    feat: enhance Navbar component with responsive design for small screens

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -49,8 +49,25 @@ const Navbar = () => {
       </div>
 
       {/* small screen resolution */}
-      <div className="">
-        
+      <div className="sm:hidden flex justify-between items-center relative">
+        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+          <img
+            src={thirdweb}
+            alt="user"
+            className="w-[60%] h-[60%] object-contain"
+          />
+          <img
+            src={menu}
+            alt="menu"
+            className="w-[34px] h-[34px] object-contain cursor-pointer"
+            onClick={() => setToggleDrawer(!toggleDrawer)}
+          />
+
+          <div
+            className={`absolute top-[60px] right-0 bg-[#1c1c24] shadow-secondary ${toggleDrawer} ? "-translate-y-[100px]" : "translate-y-0" 
+            transition-all duration-700 py-4`}
+          ></div>
+        </div>
       </div>
     </div>
   );
