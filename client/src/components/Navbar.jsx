01613691cbaebd03209714feae5baa8f49435e6d
Femzy1900 <adedokunfemi14@gmail.com>
2025-05-28 15:35:08 +0100

    hello

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
-  const { address } = useStateContext();
+  const { connect , address } = useStateContext();
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
@@ -36,7 +36,7 @@ const Navbar = () => {
           styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
           handleClick={() => {
             if (address) navigate("create-campaign");
-            else "connect";
+            else connect();
           }}
         />
 
@@ -111,7 +111,7 @@ const Navbar = () => {
               styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
               handleClick={() => {
                 if (address) navigate("create-campaign");
-                else "connect";
+                else connect();
               }}
             />
           </div>
